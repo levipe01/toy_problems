4b8bdd0b6157f93a6b8deb234d0ccc7e64bf951f
Peter Levin <levipe01@gmail.com>
2020-07-03 11:44:02 -0600

    Remove duplicate x 2

---
+++ removeDuplicatesII.js
@@ -0,0 +1,27 @@
+// Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,1,2,2,3],
+
+// Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Given nums = [0,0,1,1,1,1,2,3,3],
+
+// Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+// It doesn't matter what values are set beyond the returned length.
+
+var removeDuplicates = function(nums) {
+  let storage = {};
+  for (let i = nums.length - 1; i > -1; i--) {
+    (storage[nums[i]] === undefined) ? storage[nums[i]] = 1 : storage[nums[i]]++
+    (storage[nums[i]] > 2) ? nums.splice(i, 1) : null
+  }
+  return nums.length
+};
